9edeb5ff480e650606c7613da5c19b0c9cfd107a
chimpsye <demironat72@gmail.com>
2026-02-16 00:28:38 +0300

    Initial Commit

---
+++ SpoofDiscord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpoofDiscord
+//
+//  Created by Onat Efe Demir on 16.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpoofDiscord/SpoofDiscordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoofDiscordApp.swift
+//  SpoofDiscord
+//
+//  Created by Onat Efe Demir on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpoofDiscordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
